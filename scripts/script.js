a27c164eff3996e15a336a0b7bdc87c08f1e37d4
Alex Melnikov <alexmlnkv@gmail.com>
2021-04-22 14:11:47 +0300

    feat(script.js) выпонить ДЗ lesson19

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
-  // eslint-disable-next-line strict
-  'use strict';
+  'strict';
 
   //timer
-  let idInterval = null;
   function countTimer(deadline) {
     const timerHours = document.querySelector('#timer-hours'),
       timerMinutes = document.querySelector('#timer-minutes'),
@@ -27,7 +25,7 @@ window.addEventListener('DOMContentLoaded', () => {
         //day = Math.floor(timeRemaining / 60 / 60 / 24);
       return { timeRemaining, hours, minutes, seconds };
     }
-
+    const idInterval = setInterval(updateClock);
     function updateClock() {
       const timer = getTimeRemaining();
 
@@ -48,9 +46,69 @@ window.addEventListener('DOMContentLoaded', () => {
         });
       }
     }
-
-    updateClock();
   }
-  idInterval = setInterval(countTimer, 1000, '22 april 2021');
 
+  countTimer('23 april 2021');
+
+  //меню
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+      menu = document.querySelector('menu'),
+      closeBtn = document.querySelector('.close-btn'),
+      menuItems = menu.querySelectorAll('ul>li');
+
+    const handlerMenu = () => {
+      menu.classList.toggle('active-menu');
+    };
+
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+    menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+  };
+
+  toggleMenu();
+
+  //popup
+  const togglePopUp = () => {
+    const popup = document.querySelector('.popup'),
+      popupBtn = document.querySelectorAll('.popup-btn'),
+      popupClose = document.querySelector('.popup-close'),
+      popupContent = document.querySelector('.popup-content');
+
+    const animate = ({ timing, draw, duration }) => {
+      const start = performance.now();
+      requestAnimationFrame(function animate(time) {
+        // timeFraction изменяется от 0 до 1
+        let timeFraction = (time - start) / duration;
+        if (timeFraction > 1) timeFraction = 1;
+        // вычисление текущего состояния анимации
+        const progress = timing(timeFraction);
+        draw(progress); // отрисовать её
+        if (timeFraction < 1) {
+          requestAnimationFrame(animate);
+        }
+      });
+    };
+
+    popupBtn.forEach(elem => {
+      elem.addEventListener('click', () => {
+        if (document.documentElement.clientWidth < 768) {
+          popup.style.display = 'block';
+        } else {
+          popup.style.display = 'block';
+          popupContent.style.top = '-30%';
+          animate({
+            duration: 700,
+            timing: timeFraction => (Math.pow(timeFraction, 2) - 1.5),
+            draw: progress => popupContent.style.top = (progress * 60 + 40) + '%',
+          });
+        }
+      });
+    });
+
+    popupClose.addEventListener('click', () => {
+      popup.style.display = 'none';
+    });
+  };
+  togglePopUp();
 });
