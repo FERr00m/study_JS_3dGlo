cb6a045719f7aa4839c5e419bb3d32253635f619
Alex Melnikov <alexmlnkv@gmail.com>
2021-04-22 14:34:42 +0300

    feat(script.js) выпонить усложненное ДЗ lesson19

--- scripts/script.js
+++ scripts/script.js
@@ -111,4 +111,18 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   };
   togglePopUp();
+
+  //smoothScroll
+  const scrollLinks = document.querySelectorAll('.scroll-link');
+
+  scrollLinks.forEach(item => {
+    item.addEventListener('click', event => {
+      event.preventDefault();
+      const id = item.getAttribute('href');
+      document.querySelector(id).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    });
+  });
 });
