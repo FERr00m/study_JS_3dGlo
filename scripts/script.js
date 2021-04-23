279ffd162d5f39996896eed75e1c99abdce59242
Alex Melnikov <alexmlnkv@gmail.com>
2021-04-23 13:37:38 +0300

    feat first part

--- scripts/script.js
+++ scripts/script.js
@@ -48,31 +48,31 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  countTimer('23 april 2021');
+  countTimer('24 april 2021');
 
   //меню
   const toggleMenu = () => {
     const btnMenu = document.querySelector('.menu'),
-      menu = document.querySelector('menu'),
-      closeBtn = document.querySelector('.close-btn'),
-      menuItems = menu.querySelectorAll('ul>li');
+      menu = document.querySelector('menu');
 
-    const handlerMenu = () => {
-      menu.classList.toggle('active-menu');
+    const handlerMenu = event => {
+      const target = event.target;
+      if (target.tagName === 'A') {
+        menu.classList.toggle('active-menu');
+      }
     };
 
-    btnMenu.addEventListener('click', handlerMenu);
-    closeBtn.addEventListener('click', handlerMenu);
-    menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+    btnMenu.addEventListener('click', () => {
+      menu.classList.toggle('active-menu');
+      menu.addEventListener('click', handlerMenu);
+    });
   };
-
   toggleMenu();
 
   //popup
   const togglePopUp = () => {
     const popup = document.querySelector('.popup'),
       popupBtn = document.querySelectorAll('.popup-btn'),
-      popupClose = document.querySelector('.popup-close'),
       popupContent = document.querySelector('.popup-content');
 
     const animate = ({ timing, draw, duration }) => {
@@ -106,8 +106,16 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     });
 
-    popupClose.addEventListener('click', () => {
-      popup.style.display = 'none';
+    popup.addEventListener('click', event => {
+      let target = event.target;
+      if (target.classList.contains('popup-close')) {
+        popup.style.display = 'none';
+      } else {
+        target = target.closest('.popup-content');
+        if (!target) {
+          popup.style.display = 'none';
+        }
+      }
     });
   };
   togglePopUp();
@@ -125,4 +133,37 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     });
   });
+
+  //tabs
+  const tabs = () => {
+    const tabHeader = document.querySelector('.service-header'),
+      tab = tabHeader.querySelectorAll('.service-header-tab'),
+      tabCotent = document.querySelectorAll('.service-tab');
+
+    const toggleTabContent = index => {
+      for (let i = 0; i < tabCotent.length; i++) {
+        if (index === i) {
+          tab[i].classList.add('active');
+          tabCotent[i].classList.remove('d-none');
+        } else {
+          tab[i].classList.remove('active');
+          tabCotent[i].classList.add('d-none');
+        }
+      }
+    };
+
+    tabHeader.addEventListener('click', event => {
+      let target = event.target;
+      target = target.closest('.service-header-tab');
+
+      if (target) {
+        tab.forEach((item, i) => {
+          if (item === target) {
+            toggleTabContent(i);
+          }
+        });
+      }
+    });
+  };
+  tabs();
 });
